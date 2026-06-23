e29faac8db3712bb5122967b4d2a159b47fd5565
taykas <thaynaschaeffer@gmail.com>
2026-06-23 17:31:30 -0300

    Starting java thinggs

---
+++ Week - 01/practice/simple-exercises/question01.java
@@ -0,0 +1,12 @@
+
+
+public class question01 {
+    public static void main(String[] args) {
+
+        String nome = "Teste";
+        int idade = 21;
+        double altura =  1.50;
+        
+        System.out.println("Nome:" + nome + "\nIdade:" + idade + "\nAltura:" + altura);
+    }
+}
---
+++ Week - 01/practice/simple-exercises/question02.java
@@ -0,0 +1,9 @@
+public class question02{
+
+    public static void main(String[] args) {
+        
+        
+
+    }
+
+}
