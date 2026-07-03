ffeddafcf45cdcd638c45d6a3f156c2028a7a42c
taykas <thaynaschaeffer@gmail.com>
2026-07-03 08:28:07 -0300

    starting project

---
+++ Week - 01/practice/simple-exercises/question07.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class question07 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome:");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite sua idade  :");
+        String idade = scanner.nextInt();
+
+
+
+    }
+}
